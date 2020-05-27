88f19409610dc53dd379f00ef19759a81a7d1f65
Muhammad Abdullah <abdullah.626@outlook.com>
2020-05-27 12:57:43 +0500

    adjusted alignment of name and price in menu card

--- RS/resturant/static/resturant/index.js
+++ RS/resturant/static/resturant/index.js
@@ -28,15 +28,18 @@ function add_item(item){
     const item_div = document.createElement('div');
     const img_div = document.createElement('div');
     const image = document.createElement('img')
+    const name_div = document.createElement('div');
+    const price_div = document.createElement('div');
+    const serving_div = document.createElement('div'); 
     const name_p = document.createElement('p'); 
     const price_p = document.createElement('p'); 
     const serving_p = document.createElement('p'); 
 
     item_div.className = "menu-item-container";
     img_div.className = "item-image"
-    name_p.className = "name"
-    price_p.className = "price"
-    serving_p.className = "serving"
+    name_div.className = "name"
+    price_div.className = "price"
+    serving_div.className = "serving"
 
    
     const name = item.fields.name;
@@ -52,14 +55,19 @@ function add_item(item){
 
     console.log(image)
     name_p.innerHTML = name;
-    price_p.innerHTML = price;
-    serving_p.innerHTML = serving;
+    price_p.innerHTML = `Price: ${price}`;
+    serving_p.innerHTML = `Serving:  ${serving}`;
 
-    img_div.appendChild(image)
-    item_div.appendChild(img_div)
-    item_div.appendChild(name_p)
-    item_div.appendChild(price_p)
-    item_div.appendChild(serving_p)
+    img_div.appendChild(image);
+    item_div.appendChild(img_div);
+
+    name_div.appendChild(name_p);
+    price_div.appendChild(price_p);
+    serving_div.appendChild(serving_p)
+
+    item_div.appendChild(name_div);
+    item_div.appendChild(price_div);
+    item_div.appendChild(serving_div);
 
     document.querySelector('#menu-item').appendChild(item_div);
 }
